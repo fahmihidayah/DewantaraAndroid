4f8f58c552707a659a1f25d998e3c4cb01b77641
fahmi <vambex.system@gmail.com>
2014-09-11 17:00:16 +0700

    add user model class

---
+++ src/com/models/User.java
@@ -0,0 +1,43 @@
+package com.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	private Long id;
+	private String userName;
+	private String password;
+	private String type;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}

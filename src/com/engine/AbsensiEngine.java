fb54d197ca9e7a8c8bce47f8af987b3080530aaf
fahmi <vambex.system@gmail.com>
2014-09-13 13:27:40 +0700

    add absensi engine

---
+++ src/com/engine/AbsensiEngine.java
@@ -0,0 +1,13 @@
+package com.engine;
+
+import com.dewantara_android.AbsensiFragment;
+
+public class AbsensiEngine {
+	private AbsensiFragment absensiFragment;
+
+	public AbsensiEngine(AbsensiFragment absensiFragment) {
+		super();
+		this.absensiFragment = absensiFragment;
+	}
+
+}
